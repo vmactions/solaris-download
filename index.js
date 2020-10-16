cfacf1676eb46904257e2fab3fbc9afea01eb503
neil <win10@neilpang.com>
2020-10-16 21:24:11 +0800

    init

---
+++ index.js
@@ -0,0 +1,35 @@
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+const tc = require('@actions/tool-cache');
+const io = require('@actions/io');
+const fs = require("fs");
+
+
+async function sleep (ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+
+// most @actions toolkit packages have async methods
+async function run() {
+  try {
+
+
+    let imgName = "sol-11_4-vbox.ova";
+    let url= process.env["URL"];
+    core.info("Downloading image: " + url);
+    let img = await tc.downloadTool(url);
+    core.info("Downloaded file: " + img);
+
+    await io.mv(img, "./" + imgName);
+
+    core.info("Split file");
+    await exec.exec("zip -s 2000m sol-11_4-vbox.zip  sol-11_4-vbox.ova");
+    await exec.exec("ls -lah");
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
